35d30d291f7272b3ea851033287dd9b2ee8fad3f
Michael Brocker <49590891+TheBigMike00@users.noreply.github.com>
2020-11-07 10:08:32 -0600

    windowUpdate

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import pygame
 pygame.init()
 
+background_colour = (255,255,255) #RGB Value
+(width, height) = (800, 400) 
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Fall2020Hackathon')
+screen.fill(background_colour)
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
 class Inhabitant:
     def __init__(self, name):
         self.name = name
