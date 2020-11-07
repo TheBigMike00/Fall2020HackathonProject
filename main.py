104e420a133d75fcbffd5a971192adf19598aca1
Michael Brocker <49590891+TheBigMike00@users.noreply.github.com>
2020-11-07 11:42:23 -0600

    rectWith2D

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import pygame
 pygame.init()
 
-background_colour = (255,255,255) #RGB Value
+BLOCK_WIDTH = 20
+BLOCK_HEIGHT = 20
+
+def drawRectInArr(arrX, arrY):
+    startX = arrX * BLOCK_WIDTH
+    startY = arrY * BLOCK_HEIGHT
+    #(starting x point, starting y point, "move over x steps"", 'move /up y steps )
+    pygame.draw.rect(screen, (255,0,0), (startX, startY, BLOCK_WIDTH, BLOCK_HEIGHT))
+
+background_colour = (0,0,255) #RGB Value
 (width, height) = (800, 400) 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Fall2020Hackathon')
 screen.fill(background_colour)
+drawRectInArr(0,0)
+drawRectInArr(5,5)
+#pygame.draw.rect(screen, (255,0,0), (0,0, 40,80))
+screen.blit(screen,(0,0))
+pygame.display.flip()
+'''
+rows, cols = ((int)(height/20), (int)(width/20)) 
+arr = [[0 for i in range(cols)] for j in range(rows)] 
+arr[0][0] = 1
+print(arr)
+'''
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 class Inhabitant:
     def __init__(self, name):
