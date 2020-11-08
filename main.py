8931588052917db909912a41368d31a1ce4b618e
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2020-11-07 19:37:04 -0600

    graphics update

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ pygame.init()
 #2 -> enemy (kills player) (red)
 #3 -> player (green)
 #4 -> lava/spikes
-#5 -> 
+#5 -> exit
+#6 -> grass
 #Example: arr[2][3] = 3 (player location at x = 2 & y = 3)
 
 global DEATH_COUNTER
@@ -38,6 +39,12 @@ def updateDEATH():
     text = font.render(f"DEATHS: {DEATH_COUNTER}", True, RED, WHITE)
 
 
+def drawRectInArrBlank(color, arrX, arrY):
+    startX = arrX * BLOCK_WIDTH
+    startY = arrY * BLOCK_HEIGHT
+    #(starting x point, starting y point, "move over x steps"", 'move /up y steps )
+    pygame.draw.rect(screen, color, (startX, startY, BLOCK_WIDTH, BLOCK_HEIGHT), 1)
+
 def drawRectInArr(color, arrX, arrY):
     startX = arrX * BLOCK_WIDTH
     startY = arrY * BLOCK_HEIGHT
@@ -61,22 +68,29 @@ rows, cols = ((int)(height/20), (int)(width/20))
 #arr = [[0 for i in range(cols)] for j in range(rows)]
 #print(arr) 
 
+dirt = pygame.image.load("Dirt.png")
+dirtRect = dirt.get_rect()
+grass = pygame.image.load("GrassMid.png")
+grassRect = grass.get_rect()
+spikes = pygame.image.load("spikes.png")
+
+
 #hard coded map
 arr = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 6, 6, 6, 6, 1, 1], 
 [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
-[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 1], 
-[1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1], 
-[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 1, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 1], 
+[1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 6, 6, 6, 6, 6, 0, 0, 0, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 0, 0, 0, 0, 1, 1], 
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 5], 
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5], 
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5], 
@@ -86,14 +100,19 @@ arr = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
 def displayGameboard():
     for r in range(20):
         for c in range (40):
-            if(arr[r][c] == 0):
-                drawRectInArr(WHITE, c,r)
-            elif(arr[r][c] == 1):
+            #if(arr[r][c] == 0):
+                #drawRectInArr(WHITE, c,r)
+            if(arr[r][c] == 1):
                 drawRectInArr(BLACK, c,r)
+                screen.blit(dirt, (c*20,r*20))
             elif(arr[r][c] == 4):
-                drawRectInArr(RED, c,r)
+                drawRectInArrBlank(RED, c,r)
+                screen.blit(spikes, (c*20,r*20))
             elif(arr[r][c] == 5):
                 drawRectInArr(WINNINGCOLOR, c,r)
+            elif(arr[r][c] == 6):
+                drawRectInArr(BLACK, c,r)
+                screen.blit(grass, (c*20,r*20))
 
 displayGameboard()
 
@@ -125,7 +144,7 @@ class Player(Inhabitant):
         attemptingLocation = arr[self.ycoord][self.xcoord + 1]
         if(attemptingLocation == 0):
             currentLocation = 0
-            drawRectInArr(WHITE,self.xcoord, self.ycoord)
+            drawRectInArr(BLUE,self.xcoord, self.ycoord)
             attemptingLocation = 3
             self.xcoord = self.xcoord + 1
             self.displayPlayer()
@@ -137,7 +156,7 @@ class Player(Inhabitant):
         attemptingLocation = arr[self.ycoord][self.xcoord + 1]
         if(attemptingLocation == 0):
             currentLocation = 0
-            drawRectInArr(WHITE,self.xcoord, self.ycoord)
+            drawRectInArr(BLUE,self.xcoord, self.ycoord)
             attemptingLocation = 3
             self.xcoord = self.xcoord - 1
             self.displayPlayer()
@@ -149,7 +168,7 @@ class Player(Inhabitant):
         attemptingLocation = arr[self.ycoord - 3][self.xcoord]
         if(attemptingLocation == 0):
             currentLocation = 0
-            drawRectInArr(WHITE,self.xcoord, self.ycoord)
+            drawRectInArr(BLUE,self.xcoord, self.ycoord)
             attemptingLocation = 3
             self.ycoord = self.ycoord - 3
             self.displayPlayer()
@@ -161,17 +180,17 @@ class Player(Inhabitant):
         attemptingLocation = arr[self.ycoord + 1][self.xcoord]
         if(attemptingLocation == 0):
             currentLocation = 0
-            drawRectInArr(WHITE,self.xcoord, self.ycoord)
+            drawRectInArr(BLUE,self.xcoord, self.ycoord)
             
             pygame.draw.rect(screen, GREEN, (self.xcoord * 20, (self.ycoord * 20) + 5, BLOCK_WIDTH, BLOCK_HEIGHT))
             FramePerSec.tick(FPS)
-            pygame.draw.rect(screen, WHITE, (self.xcoord * 20, (self.ycoord * 20) + 5, BLOCK_WIDTH, BLOCK_HEIGHT))
+            pygame.draw.rect(screen, BLUE, (self.xcoord * 20, (self.ycoord * 20) + 5, BLOCK_WIDTH, BLOCK_HEIGHT))
             pygame.draw.rect(screen, GREEN, (self.xcoord * 20, (self.ycoord * 20) + 10, BLOCK_WIDTH, BLOCK_HEIGHT))
             FramePerSec.tick(FPS)
-            pygame.draw.rect(screen, WHITE, (self.xcoord * 20, (self.ycoord * 20) + 10, BLOCK_WIDTH, BLOCK_HEIGHT))
+            pygame.draw.rect(screen, BLUE, (self.xcoord * 20, (self.ycoord * 20) + 10, BLOCK_WIDTH, BLOCK_HEIGHT))
             pygame.draw.rect(screen, GREEN, (self.xcoord * 20, (self.ycoord * 20) + 15, BLOCK_WIDTH, BLOCK_HEIGHT))
             FramePerSec.tick(FPS)
-            pygame.draw.rect(screen, WHITE, (self.xcoord * 20, (self.ycoord * 20) + 15, BLOCK_WIDTH, BLOCK_HEIGHT))
+            pygame.draw.rect(screen, BLUE, (self.xcoord * 20, (self.ycoord * 20) + 15, BLOCK_WIDTH, BLOCK_HEIGHT))
             pygame.draw.rect(screen, GREEN, (self.xcoord * 20, (self.ycoord * 20) + 20, BLOCK_WIDTH, BLOCK_HEIGHT))
             FramePerSec.tick(FPS)
             
@@ -189,7 +208,7 @@ class Player(Inhabitant):
     def playerDeath(self):
         global DEATH_COUNTER
         DEATH_COUNTER = DEATH_COUNTER + 1
-        pygame.draw.rect(screen, WHITE, (self.xcoord * 20, self.ycoord * 20, BLOCK_WIDTH, BLOCK_HEIGHT))
+        pygame.draw.rect(screen, BLUE, (self.xcoord * 20, self.ycoord * 20, BLOCK_WIDTH, BLOCK_HEIGHT))
         self.xcoord = 2
         self.ycoord = 9
         self.displayPlayer()
